41c380ff0458c7cc423b7b395b4fa84996197282
carolina-tcn <carolinatacconis@gmail.com>
2024-05-30 19:00:14 +0200

    chang e main

--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,11 @@
 /*   By: carolinatacconis <carolinatacconis@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 18:20:16 by carolinatac       #+#    #+#             */
-/*   Updated: 2024/05/30 18:54:42 by carolinat        ###   ########.fr       */
+/*   Updated: 2024/05/30 18:59:37 by carolinatac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int main(void)
 {
-	Primera prieba de pull
-	printf("priemr dia de VS");
 	return (0);
 }
