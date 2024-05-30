4a6d2158eee43b654d209137ec606550472ed6f7
carolina-tcn <carolinatacconis@gmail.com>
2024-05-30 18:55:12 +0200

    main

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,12 @@
 /*   By: carolinatacconis <carolinatacconis@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 18:20:16 by carolinatac       #+#    #+#             */
-/*   Updated: 2024/05/30 18:20:36 by carolinatac      ###   ########.fr       */
+/*   Updated: 2024/05/30 18:54:42 by carolinat        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int main(void)
 {
+	printf("priemr dia de VS");
     return (0);
 }
